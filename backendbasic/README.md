0aefce9e5a5660e6c6ccdf32e9b4f71c1f809c3d
smshack <5432tat@naver.com>
2021-09-22 11:43:34 +0000

    14.유효성 검사 validation

--- backendbasic/README.md
+++ backendbasic/README.md
@@ -293,6 +293,7 @@ http://34.84.226.175:8080/sky/jarry?keyword=bts
         route => logic => data
 ## 14. Validation 유효성
 - validation, Sanitization 이란 무엇인지 , 중요성 확인하기
+    - 유효성(validation) 검사 => 클라이언트가 서버로 요청할 때 보내는 데이터가 유효한지 확인하는 것
 - Validation 실습을 통해 필요한 내용 정리하기
 - Sanitization 실습을 통해 필요한 내용 정리하기
 - Dwiiter에 어떻게 적용할 수 있을지 고민하고 적용하기
---
+++ backendbasic/validation/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const { body, param, validationResult } = require('express-validator');
+
+app.use(express.json());
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0] });
+};
+app.post(
+  '/user',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage('이름을 입력해')
+      .isLength({ min: 2 })
+      .withMessage('이름은 두글자 이상'),
+    body('age')
+      .notEmpty()
+      .withMessage('나이를 입력해')
+      .isInt()
+      .withMessage('나이는 숫자'),
+    body('email')
+      .notEmpty()
+      .withMessage('이메일을 입력해')
+      .isEmail()
+      .withMessage('이메일 형식으로 입력해 주세요'),
+    body('job.email')
+      .notEmpty()
+      .withMessage('이메일을 입력해')
+      .isEmail()
+      .withMessage('이메일 형식으로 입력해 주세요'),
+    validate,
+  ],
+  (req, res, next) => {
+    return res.sendStatus(201);
+  }
+);
+
+app.get(
+  '/:email',
+  [
+    param('email')
+      .notEmpty()
+      .withMessage('이메일을 입력해')
+      .isEmail()
+      .withMessage('이메일을 입력해요'),
+    validate,
+  ],
+  (req, res, next) => {
+    return res.sendStatus(200);
+  }
+);
+
+app.listen(30001);
